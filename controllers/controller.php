377dc9cb1aaba0139ba15b6eb543cc783542b1f3
Bramh24 <1630237@upv.edu.mx>
2019-09-26 14:44:18 -0500

    actualizacion

--- controllers/controller.php
+++ controllers/controller.php
@@ -355,6 +355,178 @@ class MvcController{
 
 
 
+
+
+
+#---------------------------------------------------
+
+
+
+#Registro de producto
+
+	public function registroProductoController(){
+
+		if(isset($_POST["codigoRegistro"])){
+
+			$datosController = array(
+				"codigo_producto"=>$_POST["codigoRegistro"], 
+
+				"nombre_producto"=>$_POST["nombreProductoRegistro"], 
+
+				"date_added"=>$_POST["fechaRegistro"],
+
+				"id_categoria"=>$_POST["categoriaRegistro"],
+
+				"precio_producto"=>$_POST["precioRegistro"],
+
+				"stock"=>$_POST["stockRegistro"]);
+
+			$respuesta = Datos::registroProductoModel($datosController, "products");
+
+			if($respuesta == "success"){
+
+				header("location:template.php?action=inventario");
+
+			}
+
+			else{
+
+				header("location:index.php");
+			}
+
+		}
+
+	}
+
+
+
+
+#------------------------------------------
+
+#Vista Producto
+
+
+	public function vistaProductosController(){
+
+		$respuesta = Datos::vistaProductosModel("products");
+
+
+		foreach($respuesta as $row => $item){
+		echo'<tr>
+				<td>'.$item["codigo_producto"].'</td>
+				<td>'.$item["nombre_producto"].'</td>
+				<td>'.$item["date_added"].'</td>
+				<td>'.$item["precio_producto"].'</td>
+				<td>'.$item["stock"].'</td>
+				<td><a href="template.php?action=editar_producto&id_producto='.$item["id_producto"].'"class="btn btn-sm btn-warning"><i class="fa fa-edit"></i></a></td>
+				<td><a href="template.php?action=inventario&idBorrar='.$item["id_producto"].'"class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a></td>
+			</tr>';
+
+		}
+	}
+
+
+
+#----------------------------------------------------
+
+
+
+	#EDITAR PRODUCTO
+	#------------------------------------
+
+	public function editarProductoController(){
+
+		$datosController = $_GET["id_producto"];
+		$respuesta = Datos::editarProductoModel($datosController, "products");
+
+		echo'
+			<div class="box-body">
+					<input type="hidden" value="'.$respuesta["id_producto"].'" name="id_productoEditar">
+
+					<div class="form-group">
+						<label for="codigoEditar">Codigo del Producto</label>
+						<input type="number" class="form-control" value="'.$respuesta["codigo_producto"].'" name="codigoEditar" required>
+					</div>
+
+					<div class="form-group">
+						<label for="nombreProductoEditar">Nombre del Producto</label>
+						<input type="text" class="form-control"  value="'.$respuesta["nombre_producto"].'" name="nombreProductoEditar" required>
+					</div>
+
+					<div class="form-group">
+						<label for="precioEditar">Precio</label>
+						<input type="number" class="form-control"  value="'.$respuesta["precio_producto"].'" name="precioEditar" required>
+					</div>
+
+					<div class="form-group">
+						<label for="stockEditar">Stock</label>
+						<input type="number" class="form-control"  value="'.$respuesta["stock"].'" name="stockEditar" required>
+					</div>
+
+					<button type="submit"  value="Actualizar" class="btn btn-flat btn-success">Actualizar</button>
+					</div>
+			 </div>';
+
+	}
+
+	#ACTUALIZAR USUARIO
+	#------------------------------------
+	public function actualizarProductoController(){
+
+		if(isset($_POST["codigoEditar"])){
+
+			$datosController = array( 
+				"id_producto"=>$_POST["id_productoEditar"],
+
+				"codigo_producto"=>$_POST["codigoEditar"],
+
+				"nombre_producto"=>$_POST["nombreProductoEditar"],
+
+				"precio_producto"=>$_POST["precioEditar"],
+
+				"stock"=>$_POST["stockEditar"]);
+			
+			$respuesta = Datos::actualizarProductoModel($datosController, "products");
+
+			if($respuesta == "success"){
+
+				header("location:template.php?action=inventario");
+
+			}
+
+			else{
+
+				echo "error";
+
+			}
+
+		}
+	
+	}
+
+
+	#BORRAR CATEGORIA
+	#------------------------------------
+	public function borrarProductoController(){
+
+		if(isset($_GET["idBorrar"])){
+
+			$datosController = $_GET["idBorrar"];
+			
+			$respuesta = Datos::borrarProductoModel($datosController, "products");
+
+			if($respuesta == "success"){
+
+				header("location:template.php?action=inventario");
+			
+			}
+
+		}
+
+	}
+
+
+
 }
 
 ?>
